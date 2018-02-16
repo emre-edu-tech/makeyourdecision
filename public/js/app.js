b3601795baeb5099064ac8b15ebc85869eb2f5af
Emre Kayikcilar <emrekayikcilar@gmail.com>
2018-02-17 01:58:10 +0300

    babel presets installed. src directory added to project

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var template = React.createElement(
+  "h1",
+  null,
+  "Make Your Decision"
+);
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,4 @@
+var template = <h1>Make Your Decision</h1>;
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
