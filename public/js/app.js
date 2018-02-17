7f12776dfff34fbb72dcb4bce3e7f3fdb0b306c8
Emre Kayikcilar <emrekayikcilar@gmail.com>
2018-02-17 12:26:30 +0300

    simple template creations done

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,53 @@
 "use strict";
 
 var template = React.createElement(
-  "h1",
-  null,
-  "Make Your Decision"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Decision Maker Application"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is some info"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item two"
+        )
+    )
+);
+
+var template2 = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Emre Kay\u0131k\xE7\u0131lar"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: 33"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: \u0130zmir"
+    )
 );
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
-var template = <h1>Make Your Decision</h1>;
+var template = (
+    <div>
+        <h1>Decision Maker Application</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+var template2 = (
+    <div>
+        <h1>Emre Kayıkçılar</h1>
+        <p>Age: 33</p>
+        <p>Location: İzmir</p>
+    </div>
+);
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot);
