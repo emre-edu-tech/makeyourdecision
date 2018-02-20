d06ae866e3076bcff7e1546a0850150757104d26
Emre Kayikcilar <emrekayikcilar@gmail.com>
2018-02-20 22:56:15 +0300

    Parent and child class exercise has been done.

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -12,7 +12,50 @@ class Person{
     }
 
     getDescription(){
-        return `${this.name} is ${this.age} year(s) old`;
+        return `${this.name} is ${this.age} year(s) old.`;
+    }
+}
+
+// create a subclass
+class Student extends Person {
+    constructor(name, age, major){
+        // super refers to the parent class
+        super(name, age);    // accessing the constructor
+        this.major = major;
+    }
+
+    hasMajor(){
+        return !!this.major;
+    }
+
+    // override the getDescription method
+    getDescription(){
+        // super keyword is used to get the parent class instance
+        let description = super.getDescription();
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}.`;
+        }
+        return description;
+    }
+}
+
+// creating a new subclass - traveller
+class Traveler extends Person{
+    constructor(name, age, homeLocation){
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    hasHomeLocation(){
+        return !!this.homeLocation;
+    }
+
+    getGreeting(){
+        let greeting = super.getGreeting();
+        if (this.hasHomeLocation()) {
+            greeting += ` I'm visiting from ${this.homeLocation}`;
+        }
+        return greeting;
     }
 }
 
@@ -23,3 +66,25 @@ console.log(me.getDescription());
 const other = new Person();
 console.log(other.getGreeting());
 console.log(other.getDescription());
+
+console.log("-------");
+
+// instances for student class
+const newStudent = new Student("Ebru Çetin", 32, "Computer Science");
+console.log(newStudent);
+console.log(newStudent.hasMajor());
+// console.log(newStudent.getGreeting());
+console.log(newStudent.getDescription());
+
+const otherStudent = new Student();
+console.log(otherStudent);
+console.log(otherStudent.hasMajor());
+console.log(otherStudent.getDescription())
+
+console.log("-------");
+
+const newTraveler = new Traveler("Emre Kayıkçılar", 33, "İzmir");
+console.log(newTraveler.getGreeting());
+
+const otherTraveler = new Traveler();
+console.log(otherTraveler.getGreeting());
